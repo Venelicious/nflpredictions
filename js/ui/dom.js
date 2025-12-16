bc82a372f01b22e83ed03af95685779f8e46272c
Venelicious <hamm.bastian@gmx.de>
2025-12-16 11:13:03 +0100

    dom.js aktualisieren

--- js/ui/dom.js
+++ js/ui/dom.js
@@ -1,1 +1,14 @@
-// Placeholder for ui/dom.js
+// ui/dom.js
+
+export const dom = {
+  authArea: document.getElementById('authArea'),
+  welcomeArea: document.getElementById('welcomeArea'),
+  tabs: document.getElementById('tabs'),
+
+  predictionsContent: document.getElementById('predictionsContent'),
+  statsContent: document.getElementById('statsContent'),
+  overviewContent: document.getElementById('overviewContent'),
+
+  savePredictions: document.getElementById('savePredictions'),
+  seasonPicker: document.getElementById('seasonPicker'),
+};
