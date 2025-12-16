90998c74291c9fa7b77a6bacf758a01761c5830a
Venelicious <hamm.bastian@gmx.de>
2025-12-16 10:41:36 +0100

    teams.js aktualisieren

--- js/data/teams.js
+++ js/data/teams.js
@@ -1,1 +1,82 @@
-// Placeholder for data/teams.js
+// data/teams.js
+
+export const CONFERENCE_ORDER = ['AFC', 'NFC'];
+export const DIVISION_ORDER = ['East', 'North', 'South', 'West'];
+export const STAT_DIVISION_ORDER = ['North', 'East', 'South', 'West'];
+
+export const teamLogos = {
+  'Buffalo Bills': 'https://a.espncdn.com/i/teamlogos/nfl/500/buf.png',
+  'Miami Dolphins': 'https://a.espncdn.com/i/teamlogos/nfl/500/mia.png',
+  'New England Patriots': 'https://a.espncdn.com/i/teamlogos/nfl/500/ne.png',
+  'New York Jets': 'https://a.espncdn.com/i/teamlogos/nfl/500/nyj.png',
+  'Baltimore Ravens': 'https://a.espncdn.com/i/teamlogos/nfl/500/bal.png',
+  'Cincinnati Bengals': 'https://a.espncdn.com/i/teamlogos/nfl/500/cin.png',
+  'Cleveland Browns': 'https://a.espncdn.com/i/teamlogos/nfl/500/cle.png',
+  'Pittsburgh Steelers': 'https://a.espncdn.com/i/teamlogos/nfl/500/pit.png',
+  'Houston Texans': 'https://a.espncdn.com/i/teamlogos/nfl/500/hou.png',
+  'Indianapolis Colts': 'https://a.espncdn.com/i/teamlogos/nfl/500/ind.png',
+  'Jacksonville Jaguars': 'https://a.espncdn.com/i/teamlogos/nfl/500/jac.png',
+  'Tennessee Titans': 'https://a.espncdn.com/i/teamlogos/nfl/500/ten.png',
+  'Denver Broncos': 'https://a.espncdn.com/i/teamlogos/nfl/500/den.png',
+  'Kansas City Chiefs': 'https://a.espncdn.com/i/teamlogos/nfl/500/kc.png',
+  'Las Vegas Raiders': 'https://a.espncdn.com/i/teamlogos/nfl/500/lv.png',
+  'Los Angeles Chargers': 'https://a.espncdn.com/i/teamlogos/nfl/500/lac.png',
+  'Dallas Cowboys': 'https://a.espncdn.com/i/teamlogos/nfl/500/dal.png',
+  'New York Giants': 'https://a.espncdn.com/i/teamlogos/nfl/500/nyg.png',
+  'Philadelphia Eagles': 'https://a.espncdn.com/i/teamlogos/nfl/500/phi.png',
+  'Washington Commanders': 'https://a.espncdn.com/i/teamlogos/nfl/500/wsh.png',
+  'Chicago Bears': 'https://a.espncdn.com/i/teamlogos/nfl/500/chi.png',
+  'Detroit Lions': 'https://a.espncdn.com/i/teamlogos/nfl/500/det.png',
+  'Green Bay Packers': 'https://a.espncdn.com/i/teamlogos/nfl/500/gb.png',
+  'Minnesota Vikings': 'https://a.espncdn.com/i/teamlogos/nfl/500/min.png',
+  'Atlanta Falcons': 'https://a.espncdn.com/i/teamlogos/nfl/500/atl.png',
+  'Carolina Panthers': 'https://a.espncdn.com/i/teamlogos/nfl/500/car.png',
+  'New Orleans Saints': 'https://a.espncdn.com/i/teamlogos/nfl/500/no.png',
+  'Tampa Bay Buccaneers': 'https://a.espncdn.com/i/teamlogos/nfl/500/tb.png',
+  'Arizona Cardinals': 'https://a.espncdn.com/i/teamlogos/nfl/500/ari.png',
+  'Los Angeles Rams': 'https://a.espncdn.com/i/teamlogos/nfl/500/lar.png',
+  'San Francisco 49ers': 'https://a.espncdn.com/i/teamlogos/nfl/500/sf.png',
+  'Seattle Seahawks': 'https://a.espncdn.com/i/teamlogos/nfl/500/sea.png',
+};
+
+export const teams = [
+  { name: 'Buffalo Bills', conference: 'AFC', division: 'East' },
+  { name: 'Miami Dolphins', conference: 'AFC', division: 'East' },
+  { name: 'New England Patriots', conference: 'AFC', division: 'East' },
+  { name: 'New York Jets', conference: 'AFC', division: 'East' },
+
+  { name: 'Baltimore Ravens', conference: 'AFC', division: 'North' },
+  { name: 'Cincinnati Bengals', conference: 'AFC', division: 'North' },
+  { name: 'Cleveland Browns', conference: 'AFC', division: 'North' },
+  { name: 'Pittsburgh Steelers', conference: 'AFC', division: 'North' },
+
+  { name: 'Houston Texans', conference: 'AFC', division: 'South' },
+  { name: 'Indianapolis Colts', conference: 'AFC', division: 'South' },
+  { name: 'Jacksonville Jaguars', conference: 'AFC', division: 'South' },
+  { name: 'Tennessee Titans', conference: 'AFC', division: 'South' },
+
+  { name: 'Denver Broncos', conference: 'AFC', division: 'West' },
+  { name: 'Kansas City Chiefs', conference: 'AFC', division: 'West' },
+  { name: 'Las Vegas Raiders', conference: 'AFC', division: 'West' },
+  { name: 'Los Angeles Chargers', conference: 'AFC', division: 'West' },
+
+  { name: 'Dallas Cowboys', conference: 'NFC', division: 'East' },
+  { name: 'New York Giants', conference: 'NFC', division: 'East' },
+  { name: 'Philadelphia Eagles', conference: 'NFC', division: 'East' },
+  { name: 'Washington Commanders', conference: 'NFC', division: 'East' },
+
+  { name: 'Chicago Bears', conference: 'NFC', division: 'North' },
+  { name: 'Detroit Lions', conference: 'NFC', division: 'North' },
+  { name: 'Green Bay Packers', conference: 'NFC', division: 'North' },
+  { name: 'Minnesota Vikings', conference: 'NFC', division: 'North' },
+
+  { name: 'Atlanta Falcons', conference: 'NFC', division: 'South' },
+  { name: 'Carolina Panthers', conference: 'NFC', division: 'South' },
+  { name: 'New Orleans Saints', conference: 'NFC', division: 'South' },
+  { name: 'Tampa Bay Buccaneers', conference: 'NFC', division: 'South' },
+
+  { name: 'Arizona Cardinals', conference: 'NFC', division: 'West' },
+  { name: 'Los Angeles Rams', conference: 'NFC', division: 'West' },
+  { name: 'San Francisco 49ers', conference: 'NFC', division: 'West' },
+  { name: 'Seattle Seahawks', conference: 'NFC', division: 'West' },
+];
